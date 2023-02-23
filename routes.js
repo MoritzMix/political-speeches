6249dfb5fe78700cfb3a9c16e8808df2f64d7ff7
Moritz Mix <moritz.mix@web.de>
2023-02-23 16:29:23 +0100

    cleanup

--- routes.js
+++ routes.js
@@ -1,23 +1,22 @@
 "use strict";
 
 const axios = require("axios");
-const csv=require('csvtojson');
-const Enumerable = require("linq");
+const csv = require('csvtojson');
 
-module.exports = async function(app){
+module.exports = async function (app) {
 
     app.get('/evaluation', async (req, res) => {
         const urls = Object.values(req.query);
         const content = await getUrlContents(urls)
         let data = []
 
-        for await (const el of content) {         
-            if(el.status ==="fulfilled"){
+        for await (const el of content) {
+            if (el.status === "fulfilled") {
                 data = data.concat(await convertCsvToJson(el.value.data))
             }
         }
 
-        if(data.length === 0) {
+        if (data.length === 0) {
             res.status(500).send("No csv-files found")
         } else {
             res.send(JSON.stringify(evaluateJson(data)));
@@ -25,46 +24,90 @@ module.exports = async function(app){
     })
 }
 
- async function getUrlContents(urls) {
-    const requests = urls.map((url) => axios.get(url,{responseType: "blob"}));
+async function getUrlContents(urls) {
+    const requests = urls.map((url) => axios.get(url, { responseType: "blob" }));
     return await Promise.allSettled(requests);
 }
 
-async function convertCsvToJson(csvString){
-    return await csv({
+async function convertCsvToJson(csvString) {
+    const jsonData = await csv({
         noheader: false,
         output: "json"
     })
-    .fromString(csvString)
-    .then((jsonData)=>{ 
-        return jsonData;
-    })
+        .fromString(csvString)
+    return jsonData;
 }
 
-function evaluateJson(jsonData){
+function evaluateJson(jsonData) {
 
-    const mostSpeeches = Enumerable.from(jsonData)
-        .where("new Date($.Date).getUTCFullYear() == 2013")
-        .groupBy("$.Speaker", "$.Speaker",  (key, group)=>({"Speaker": key, length: group.count()}), (key)=>key.toString())
-        .orderBy("$.length")
-        .toJSONString();
+    const Speakers = {};
 
-    const mostSecurity = Enumerable.from(jsonData)
-        .where("$.Topic == 'Internal Security'")
-        .groupBy("$.Speaker", "$.Speaker",  (key, group)=>({"Speaker": key, length: group.count()}), (key)=>key.toString())
-        .orderBy("$.length")
-        .toJSONString();
-   
-    const leastWordy = Enumerable.from(jsonData)
-        .groupBy("$.Speaker", "parseInt($.Words)",  (key, group)=>({"Speaker": key, length: group.sum()}), (key)=>key.toString())
-        .orderBy("$.length")
-        .toJSONString();
+    const evaluateDate = (speech) => {
+        return speech.indexOf("2013") === 0 ? 1 : 0;
+    };
 
-   return {
-        mostSpeeches: JSON.parse(mostSpeeches).pop()?.Speaker || null,
-        mostSecurity: JSON.parse(mostSecurity).pop()?.Speaker || null,
-        leastWordy: JSON.parse(leastWordy)[0]?.Speaker || null,
-     }
+    const evaluateTopic = (topic) => {
+        return topic.toLowerCase() === "internal security" ? 1 : 0;
+    };
 
+    jsonData.forEach((el) => {
+        const existingSpeaker = Speakers[el.Speaker];
+        if (existingSpeaker) {
+            Object.assign(existingSpeaker, {
+                words: existingSpeaker.words + Number(el.Words),
+                speeches: existingSpeaker.speeches + evaluateDate(el.Date),
+                topic: existingSpeaker.topic + evaluateTopic(el.Topic),
+            });
 
+        } else {
+            Speakers[el.Speaker] = {
+                words: Number(el.Words),
+                speeches: evaluateDate(el.Date),
+                topic: evaluateTopic(el.Topic),
+            };
+        }
+    });
+
+    const result = {
+        mostSpeeches: null,
+        mostSecurity: null,
+        leastWordy: null,
+    };
+
+    Object.entries(Speakers).forEach((speaker) => {
+        if (
+            speaker[1].speeches > 0 &&
+            speaker[1].speeches >
+            (result.mostSpeeches ? result.mostSecurity[1].speeches : 0)
+        ) {
+            result.mostSpeeches = speaker;
+        }
+
+        if (
+            speaker[1].topic > 0 &&
+            speaker[1].topic > (result.mostSecurity ? result.mostSecurity[1].topic : 0)
+        ) {
+            result.mostSecurity = speaker;
+        }
+
+        if (
+            speaker[1].words > 0 &&
+            speaker[1].words <
+            (result.leastWordy ? result.leastWordy[1].words : Number.MAX_SAFE_INTEGER)
+
+        ) {
+            result.leastWordy = speaker;
+        }
+
+    });
+
+    const returnNameifExisting = (speaker) => {
+        return speaker ? speaker[0] : null;
+    };
+
+    return {
+        mostSpeeches: returnNameifExisting(result.mostSpeeches),
+        mostSecurity: returnNameifExisting(result.mostSecurity),
+        leastWordy: returnNameifExisting(result.leastWordy),
+    };
 }
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const express = require('express')
-const https = require('https');
 const http = require('http');
 
 const app = express()
@@ -10,20 +9,12 @@ const port_https = 5000;
 
 require('./routes')(app);
 
-const options = {                                     
-}
-
 //Mock URLs
 app.use(express.static('public'));
 
-https
-  .createServer(options, app)
-  .listen(port_https , ()=>{
-    console.log(`https listening on port ${port_https}`)
-});
 
 http
-    .createServer(app)
-  .listen(port_http , ()=>{
+  .createServer(app)
+  .listen(port_http, () => {
     console.log(`http listening on port ${port_http}`)
   });
